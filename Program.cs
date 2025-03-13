fcbd3d3223f035cb1623e05cd32c61ce66ab8cdd
Ash <aSHLEYMILLS500@OUTLOOK.COM>
2025-03-13 11:09:50 +0000

    removed waiting for implementation

--- Program.cs
+++ Program.cs
@@ -13,8 +13,6 @@ namespace DungeonExplorer
         {
             Game game = new Game();
             game.Start();
-            Console.WriteLine("Waiting for your Implementation");
-            Console.ReadKey();
         }
     }
 }
